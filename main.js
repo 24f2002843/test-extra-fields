0ebf04c300677c2a93491f8c4088f254960219da
24f2002843 <24f2002843@ds.study.iitm.ac.in>
2025-10-15 00:36:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM fully loaded and parsed');
     // Here we will test how extra fields interact with our code
+    const extraFields = document.getElementById('extra-fields');
+    extraFields.innerHTML = '<p>Extra Fields:</p><ul><li>Field 1: Test</li><li>Field 2: Example</li></ul>';
 });
